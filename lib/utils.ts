69771f1a4c9e229340c14d98f021246d88b62ffc
shorabh-123 <shorabh@mashrmail.com>
2026-01-29 20:53:16 +0530

    frontend UI

--- lib/utils.ts
+++
@@ -1,8 +0,0 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export { cn } from "@/lib/utils"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
